ec11e00d79ef6268c0362f161ba3411351fe20db
luzonghao1 <luzonghao1@qq.com>
2015-08-07 08:52:06 +0800

    three bean staff.java DStaffEducation.java DStaffWorkExperence.java.

---
+++ src/com/hqgj/xb/bean/DStaffEducation.java
@@ -0,0 +1,73 @@
+package com.hqgj.xb.bean;
+
+/**
+ * @author 鲁宗豪
+ * @datetime 2015年8月6日 下午9:17:05
+ */
+public class DStaffEducation {
+	
+	private String id;
+	private String userId;
+	private String startDate;
+	private String endDate;
+	private String school;
+	private String unified;
+	private String major;
+	private String education;
+	
+	public  DStaffEducation() {
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getUnified() {
+		return unified;
+	}
+	public void setUnified(String unified) {
+		this.unified = unified;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	
+	
+	
+}
